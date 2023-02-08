6ef609e3216ff940e6f33b7346051c358187bb42
Sameer <sksameer204@gmail.com>
2023-02-08 20:57:56 +0530

    Added feature task

---
+++ src/containers/home/HomeLayout.tsx
@@ -0,0 +1,91 @@
+import {
+  Container,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  Heading,
+  TabProps,
+  Box,
+  Grid,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import InterviewSettingsForm from "./InterviewSettingsForm";
+import JobDetailsForm from "./JobDetailsForm";
+import RequisitionForm from "./RequisitionDetailsForm";
+import DisplayCard from "./PreviewCard";
+import { PageNumbers } from "../../interface/home";
+
+
+const CustomTab: React.FC<TabProps> = ({ children, ...props }) => {
+  return (
+    <Tab p="1rem" fontFamily="Poppins" {...props}>
+      {children}
+    </Tab>
+  );
+};
+
+const HomeLayout = () => {
+  const [page, setPage] = useState<PageNumbers>(0);
+
+  const handlePage = (pageNumber: PageNumbers) => {
+    setPage(pageNumber);
+  };
+
+  // data
+  const [requisitionForm, setRequisitionForm] = useState({
+    requisitionTitle: '', noOfOpenings: '', urgency: "", gender: ""
+  });
+
+  const [jobDetailsForm, setJobDetailsForm] = useState({
+    jobTitle: '', jobDetails: '', jobLocation: ''
+  });
+  const [interviewSettingsForm, setInterviewSettingsForm] = useState({
+    interviewMode: '', interviewDuration: '', interviewLanguage: ''
+  });
+
+
+  // const requisitionDetails = (childFormValues) => {
+  //   // setData(childFormValues)
+  //   console.log(childFormValues)
+  // };
+
+
+  return (
+    <Box w="100%">
+      <Container maxW="1200px">
+        <Heading fontFamily="Poppins" fontSize="1.5rem" my="2rem">
+          Create Candidate Requisition
+        </Heading>
+        <Tabs index={page} isLazy lazyBehavior="keepMounted">
+          <TabList>
+            <CustomTab>Requistion Details</CustomTab>
+            <CustomTab>Job Details</CustomTab>
+            <CustomTab>Interview Settings</CustomTab>
+          </TabList>
+          <Grid display="grid" gridTemplateColumns="3fr 2fr" gap="24px">
+            <TabPanels>
+              <TabPanel>
+                <RequisitionForm handleTab={handlePage} setRequisitionForm={setRequisitionForm} />
+              </TabPanel>
+              <TabPanel>
+                <JobDetailsForm handleTab={handlePage} setJobDetailsForm={setJobDetailsForm} />
+              </TabPanel>
+              <TabPanel>
+                <InterviewSettingsForm handleTab={handlePage} setInterviewSettingsForm={setInterviewSettingsForm} />
+              </TabPanel>
+            </TabPanels>
+            <DisplayCard
+              requisitionDetails={requisitionForm}
+              jobDetails={jobDetailsForm}
+              interviewSettings={interviewSettingsForm}
+            />
+          </Grid>
+        </Tabs>
+      </Container>
+    </Box>
+  );
+};
+
+export default HomeLayout;
---
+++ src/interface/home.ts
@@ -0,0 +1,2 @@
+export type PageNumbers = 0 | 1 | 2;
+
